17c4cdc659b60eda8274e846df24a842a3a037c6
Jian Luo <crackedpoly@outlook.com>
2023-11-29 15:07:46 +0800

    chore: make Node printable

--- src/main/java/org/nextlab/tobdd/Node.java
+++ src/main/java/org/nextlab/tobdd/Node.java
@@ -1,5 +1,6 @@
 package org.nextlab.tobdd;
 
+import java.util.StringTokenizer;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Node {
@@ -25,5 +26,21 @@ public class Node {
 
     public int refCount() {
         return refCount.get();
+    }
+
+    private String _toString(int tabLevel) {
+        if (low == null && high == null) {
+            return "Terminal: " + (level == Integer.MAX_VALUE ? "FALSE" : "TRUE");
+        } else {
+            return "Node {\n" +
+                    "  ".repeat(tabLevel) + "level: " + level + "\n" +
+                    "  ".repeat(tabLevel) + "low: " + low._toString(tabLevel+1) + "\n" +
+                    "  ".repeat(tabLevel) + "high: " + high._toString(tabLevel+1) + "\n" +
+                    "  ".repeat(tabLevel-1) + "}";
+        }
+    }
+
+    public String toString() {
+        return _toString(1);
     }
 }
